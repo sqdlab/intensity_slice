a0e16e1efeb872eec9ae0b3f92dec2d664ed8e45
sqdlab <40754361+sqdlab@users.noreply.github.com>
2018-09-14 15:43:03 +1000

    Update run_intensity.bat
    Add comments. Change such that it can open files by clicking on the ".dat" file.

--- intensity_slice/run_intensity.bat
+++ intensity_slice/run_intensity.bat
@@ -1,2 +1,10 @@
-C:\Users\Lachlan\Anaconda2\envs\intensity\python.exe intensity_slice_gui.py "C:\Users\Lachlan\Google Drive\current\SCIE3011\intensity_slice_background\plot_files\data\TVMode.dat"
+::This instruction explains how to setup batch file if you have Anaconda installation
+::Check that the python.exe is added to the systems paths: open command prompt and type "python" it should return the python version which corresponds to the Anaconda installation (like "Python 2.7.15|Anaconda,Inc.| ...")
+::In this case the path is already added. 
+::Otherwise type "where python" in the Anaconda prompt and it will return the path to the "python.exe" file. Add this path to the system variable "Path" 
+::(type "system variables" in Windows search and go to "Edit System Variables" option. Got to "Environment Variables" in the "System variables" click on "Path" and a new path copied from Anaconda prompt.
+::After that you can use the bath file as default program to open ".dat" files. Replace the path "C:\Software\intensity_slice-master\intensity_slice\intensity_slice_gui.py" with the correpsonding path on your computer
+::"%~1" is used to supply the name of the file even if there are spaces in the path
+
+python.exe C:\Software\intensity_slice-master\intensity_slice\intensity_slice_gui.py "%~1"
 pause
